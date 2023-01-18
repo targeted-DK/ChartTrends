ad6f42bdaa34d212b71f722afe01e920c50b8b55
DK <targeted.dk@gmail.com>
2023-01-18 17:43:09 +0900

    adding cors and express package

--- src/js/fred.js
+++ src/js/fred.js
@@ -1,12 +1,23 @@
 import axios from "axios";
-import cors from "cors"
 const APIKEY = "c4c4022663dafa850bc174cd583b0579";
 const FILE_TYPE = "json";
 const proxy = "https://cryptic-headland-94862.herokuapp.com/";
 axios.defaults.withCredentials = true; 
 import Fred from 'node-fred'
 
+var express = require('express')
+var cors = require('cors')
+var app = express()
 
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 
 const fred = new Fred(APIKEY);
